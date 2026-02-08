608317a0ee9c0666aee90d8e810af7b465fa9306
max <maximilien.ilic@gmail.com>
2026-02-08 18:20:41 +0100

    ajout final colere et satisfaction

--- src/intro.js
+++ src/intro.js
@@ -1,11 +1,13 @@
 const button = document.getElementById("button");
-
-
+const intro = document.getElementById("intro");
+const principal = document.getElementById('principale');
 button.addEventListener("click", function() {
   this.classList.add("click");
-  document.body.classList.add("zoom-out");
-  
+  intro.classList.add("zoom-out");
+    principal.play().catch(e => {
+    console.warn("Impossible de jouer le son :", e);
+  });
   setTimeout(() => {
-    window.location.href = "index.html";
+intro.style.display = "none";
   }, 800);
 });
